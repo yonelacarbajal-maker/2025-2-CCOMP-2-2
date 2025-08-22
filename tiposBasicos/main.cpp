bd23a3ccbbd27f157f8285805d41da5f9e97c385
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:23:38 -0500

    Semana 2 tipos basicos

---
+++ tiposBasicos/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val1 = 10 ;
+    int val2 = 10 ;
+    int result = val1 + val2;
+    cout << "EL resultado es" << result << endl;
+    return 0;
+}
